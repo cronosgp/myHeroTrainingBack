689df05c59fead0066444cab1ba8ed74630ce1c4
Gabriel Gomes Pereira da Silva <gabriel.gomes@ibm.com>
2021-02-10 17:22:58 -0300

    to master

---
+++ src/main/java/com/ifsp/MyHeroTraining/Models/Notificacao.java
@@ -0,0 +1,50 @@
+package com.ifsp.MyHeroTraining.Models;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+@Entity
+//userDetails serve para dizer ao spring que essa entidade representa um usuário para autentocacap
+public class Notificacao{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String tipo;
+    private String conteudo;
+    @ManyToOne
+    private Usuario usuario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
