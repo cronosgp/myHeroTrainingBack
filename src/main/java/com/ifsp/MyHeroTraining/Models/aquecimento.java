864aa91c8b02727bfdb18716ffcd5f0e7e088749
larissaCarvalho23 <carvalholb95@hotmail.com>
2021-02-25 18:39:44 -0300

    ajuste finais

--- src/main/java/com/ifsp/MyHeroTraining/Models/aquecimento.java
+++ src/main/java/com/ifsp/MyHeroTraining/Models/aquecimento.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class treinoPersonalizado {
+public class aquecimento {
     public int getId() {
         return id;
     }
--- src/main/java/com/ifsp/MyHeroTraining/repository/TreinoPersonalizadoRepository.java
+++ src/main/java/com/ifsp/MyHeroTraining/repository/TreinoPersonalizadoRepository.java
@@ -1,6 +1,6 @@
 package com.ifsp.MyHeroTraining.repository;
 
-import com.ifsp.MyHeroTraining.Models.treinoPersonalizado;
+import com.ifsp.MyHeroTraining.Models.aquecimento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 import javax.transaction.Transactional;
 import java.util.List;
 
-public interface TreinoPersonalizadoRepository extends JpaRepository<treinoPersonalizado, Integer> {
+public interface aquecimentoRepository extends JpaRepository<aquecimento, Integer> {
 
 
    @Transactional
     @Query(value ="SELECT * FROM treino_personalizado AS ex JOIN treino_personalizado_usuario as ps " +
             "ON PS.id_exercicio = ex.id " +
             "where ps.idusuario = :id",nativeQuery = true)
-   List<treinoPersonalizado> findByIdusuario(@Param("id") int id);
+   List<aquecimento> findByIdusuario(@Param("id") int id);
 
 }
